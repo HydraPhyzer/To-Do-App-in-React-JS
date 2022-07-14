13794afa7bb98a9fe7f969d6a20e2311c5597329
Muhammad Zubair <2020cs601@student.uet.edu.pk>
2022-07-14 11:05:36 +0500

    Now We Can Add & Delete To Do (LocalStorage Step)

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-import { useState } from 'react'
+import { useEffect, useState} from 'react'
 import './App.css'
 import Form from './Components/Form'
 import Todo from './Components/Todo'
 
 let App = () => {
     let [ToDoArray, setToDoArray] = useState([]);
+
     let CollectInput = (Value) => 
     {
         let Temp=[Value,...ToDoArray];
+        localStorage.setItem("Todo",JSON.stringify(Temp));
         setToDoArray(Temp);
     }
+
     let Delete=(Ind)=>
     {
         let newArr=[...ToDoArray];
         newArr.splice(Ind,1);
+        localStorage.setItem("Todo",JSON.stringify(newArr));
         setToDoArray(newArr);
     }
+    useEffect(()=>
+    {
+        let Ar=JSON.parse(localStorage.getItem("Todo"));
+        if(Ar)
+        setToDoArray(Ar);
+    },[])
     return (
         <div className='App'>
             <div className="App-Content">
