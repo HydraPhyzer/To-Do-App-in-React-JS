cf4856f1a6ec2df3484c16666ffa7de5bb77494f
Muhammad Zubair <2020cs601@student.uet.edu.pk>
2022-07-14 07:51:18 +0500

    Initial Commit

---
+++ src/Components/Form.js
@@ -0,0 +1,15 @@
+import './Form.css'
+
+let Form=()=>
+{
+    return(
+        <div className="Form">
+            <input type="text" />
+            <div className='Plus'>
+                <i className="fa-solid fa-plus"></i>
+            </div>
+        </div>
+    )
+}
+
+export default Form
